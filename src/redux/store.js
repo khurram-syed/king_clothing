ff8dea53b19367e3c7e08d065421a5b2e18f2176
khurram-syed <khurram245@yahoo.com>
2020-02-27 13:35:17 +0000

    KSyed : updated logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,11 @@ import {createStore, applyMiddleware} from 'redux'
 import {logger} from 'redux-logger'
 import rootPersistReducer from './root.reducer'
 import {persistStore} from 'redux-persist'
+
+const middleware = []
+if(process.env.NODE_ENV!=="production"){
+    middleware.push(logger)
+}
  const middleware = [logger]
 export const store = createStore(rootPersistReducer,applyMiddleware(...middleware))
 export const persistor = persistStore(store);
